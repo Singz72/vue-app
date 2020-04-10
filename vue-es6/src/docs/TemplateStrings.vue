77e26830a220d49311ceec260353dcaeaa24be98
huajing <huajing@microants.cn>
2020-04-10 09:03:28 +0800

    feat:模版字符串

--- vue-es6/src/docs/TemplateStrings.vue
+++ vue-es6/src/docs/TemplateStrings.vue
@@ -1,10 +1,18 @@
 <template>
-  <div class="template_strings">template_strings</div>
+  <div class="template_strings">
+    <span v-html="content" v-highlightInserted></span>
+  </div>
 </template>
 
 <script>
+import content from "../md/single/template_strings.md";
 export default {
-  name: "template_strings"
+  name: "template_strings",
+  data() {
+    return {
+      content
+    };
+  }
 };
 </script>
 
