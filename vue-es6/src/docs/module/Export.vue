6f24b9832139bb6770a4b125875fab819794c12e
huajing <huajing@microants.cn>
2020-04-15 14:05:18 +0800

    feat:export

--- vue-es6/src/docs/module/Export.vue
+++ vue-es6/src/docs/module/Export.vue
@@ -1,10 +1,18 @@
 <template>
-  <div class="export">export</div>
+  <div class="export">
+    <span v-html="content" v-highlightInserted></span>
+  </div>
 </template>
 
 <script>
+import content from "../../md/module/export.md";
 export default {
-  name: "exportType"
+  name: "exportType",
+  data() {
+    return {
+      content
+    };
+  }
 };
 </script>
 
