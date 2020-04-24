036c9e0095f7d10a6c060ef127932d46067a49b6
huajing <huajing@microants.cn>
2020-04-24 23:52:38 +0800

    promise

--- vue-es6/src/docs/Promise.vue
+++ vue-es6/src/docs/Promise.vue
@@ -1,10 +1,18 @@
 <template>
-  <div class="promise">promise</div>
+  <div class="promise">
+    <span v-html="content" v-highlightInserted></span>
+  </div>
 </template>
 
 <script>
+import content from "../md/single/promise.md";
 export default {
-  name: "promiseType"
+  name: "promiseType",
+  data() {
+    return {
+      content
+    };
+  }
 };
 </script>
 
