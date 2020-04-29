dcefb4b799664e7f50c5bb07917a3edec3abd351
huajing <huajing@microants.cn>
2020-04-29 23:22:52 +0800

    Set对象

--- vue-es6/src/docs/primitive_data_type/Set.vue
+++ vue-es6/src/docs/primitive_data_type/Set.vue
@@ -1,10 +1,18 @@
 <template>
-  <div class="set">set</div>
+  <div class="set">
+    <span v-html="content" v-highlightInserted></span>
+  </div>
 </template>
 
 <script>
+import content from "../../md/primitive_data_type/Set.md";
 export default {
-  name: "setType"
+  name: "setType",
+  data() {
+    return {
+      content
+    };
+  }
 };
 </script>
 
